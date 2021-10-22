4db2e1d5a2326a4dd33a45b9932e5730e8597fef
maoxiaoke <maoxiaoke@outlook.com>
2021-10-22 17:15:15 +0800

    feat: 🎸 update deps

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vite + icestark + React Demo
+
+Demo for building icestark micro applications created by Vite.
+
+## Online
+
+https://icestark-vite-react.vercel.app/
+
+
+[Edit in stackBlitz](https://stackblitz.com/edit/github-kpsfcv)
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import htmlPlugin from 'vite-plugin-htmls'
+import htmlPlugin from 'vite-plugin-index-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
